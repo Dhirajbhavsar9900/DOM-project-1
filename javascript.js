8dc6018c8580ede8640f64f2e55f86f74f6d90b3
Dhiraj Bhawsar <dhirajbhavsar.official@gmail.com>
2024-02-28 09:21:58 +0000

    uploading project

---
+++ javascript.js
@@ -0,0 +1,12 @@
+var div = document.getElementById("divSection");
+div.style.backgroundColor = "red"; 
+div.style.color = "black";
+div.style.margin = "30px";           
+div.style.padding = "50px";         
+div.style.fontSize = "30px";        
+div.style.fontWeight = "bold";      
+div.style.height = "100px";         
+div.style.width = "400px";         
+div.style.textAlign= "center";
+div.style.textDecoration = "underline";   
+
